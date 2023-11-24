cadd434f3a1249c919ca8b314ee6eda759b0812e
Messitos16 <149245706+Messitos16@users.noreply.github.com>
2023-11-24 17:39:18 +0300

    Add files via upload

---
+++ 5/script.js
@@ -0,0 +1,11 @@
+
+function createCircle() {
+   var circle = document.createElement("div");
+  circle.style.width = "100px";
+  circle.style.height = "100px";
+  circle.style.borderRadius = "50%";
+  circle.style.backgroundColor = "purple";
+  
+ 
+  document.body.appendChild(circle);
+}
